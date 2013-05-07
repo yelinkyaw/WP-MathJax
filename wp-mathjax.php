4761449eeb8871bc8b5500e955679a7156829220
Ye Lin Kyaw <yelinkyaw@gmail.com>
2013-05-07 23:26:33 +0800

    Add configuration to admin panel

--- wp-mathjax.php
+++ wp-mathjax.php
@@ -8,12 +8,20 @@ Version: 0.1.0
 Author URI: http://www.yelinkyaw.com
 */
 
+//Initialization
+if (get_option('wp_mathjax_local') =="")
+{
+	update_option('wp_mathjax_local', 1);
+	update_option('wp_mathjax_config', 'TeX-AMS-MML_HTMLorMML');
+}
+
 // Add MathJax to Header
 add_action('wp_head', 'mathjax');
 
 // MathJax Header
 function mathjax()
 {
+	// Local or CDN MathJax Javascript
 	if(get_option('wp_mathjax_local')=='1')
 	{
 		$mathjax_url = plugins_url('MathJax/MathJax.js', __FILE__);
@@ -22,7 +30,8 @@ function mathjax()
 	{
 		$mathjax_url = 'http://cdn.mathjax.org/mathjax/latest/MathJax.js';
 	}
-	$config = 'TeX-AMS-MML_HTMLorMML';
+	// Get MathJax Config
+	$config = get_option('wp_mathjax_config');
 	echo "<script type=\"text/javascript\" src=\"$mathjax_url?config=$config\"></script>\n";
 }
 
@@ -41,13 +50,6 @@ function admin_options()
 	{
 		update_option('wp_mathjax_local', $_POST['wp_mathjax_local']);
 	}
-	
-	//Initialization
-	if (get_option('wp_mathjax_local') =="")
-	{
-		update_option('wp_mathjax_local', 1);
-	}
-	
 	//Show Admin UI
 	admin_ui();
 }
@@ -57,14 +59,42 @@ function admin_options()
 function admin_ui()
 {
 ?>
-<h2>WP MathJax</h2>
+<h2>WP MathJax Settings</h2>
 <form method="post" action="options-general.php?page=wp-mathjax.php">
 	<table>
 		<tr>
-			<td>MathJax Local:</td><td><input type="radio" name="wp_mathjax_local" value="1" <?php if(get_option('wp_mathjax_local')=='1') echo 'checked="checked"'; ?>/></td>
+			<td colspan="2"><h3>MathJax Javascript Location</h3></td>
 		</tr>
 		<tr>
-			<td>MathJax CDN:</td><td><input type="radio" name="wp_mathjax_local" value="0" <?php if(get_option('wp_mathjax_local')=='0') echo 'checked="checked"'; ?>/></td>
+			<td>Local:</td><td><input type="radio" name="wp_mathjax_local" value="1" <?php if(get_option('wp_mathjax_local')=='1') echo 'checked="checked"'; ?>/></td>
+		</tr>
+		<tr>
+			<td>CDN:</td><td><input type="radio" name="wp_mathjax_local" value="2" <?php if(get_option('wp_mathjax_local')=='2') echo 'checked="checked"'; ?>/></td>
+		</tr>
+		<tr>
+			<td colspan="2"><h3>MathJax Configuration</h3></td>
+		</tr>
+		<tr>
+			<?php
+			$config_dir = plugin_dir_path(__FILE__).'MathJax/config/';
+			$files = scandir($config_dir);
+			
+			foreach($files as $file)
+			{
+				if(is_file($file))
+				{
+					$path_parts = pathinfo($file);
+					$config_file = $path_parts['filename'];
+					$checked = '';
+					if(get_option('wp_mathjax_config')==$config_file)
+					{
+						$checked = 'checked="checked"';
+					}
+					echo "<td>$config_file:</td><td><input type=\"radio\" name=\"wp_mathjax_config\" value=\"$config_file\" $checked/></td>";
+				}
+			}
+			?>
+			<td>Custom:</td><td><input type="radio" name="wp_mathjax_config" value="custom" <?php if(get_option('wp_mathjax_config')=='custom') echo 'checked="checked"'; ?>/></td>
 		</tr>
 	</table>
 	<input type="submit" name="Submit" value="Save Settings" />		
